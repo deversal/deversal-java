c135f7a3e5b1bc00eb2afca7affa8a40534360c2
Cypheyr <Cypheyr@gmail.com>
2016-11-03 18:21:57 -0400

    Add functionality to Device.java to match the Python version

--- Device.java
+++ Device.java
@@ -1,15 +1,42 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class Device {
 
     // Variables
     String address;
+    String bridgeURI;
+    String deviceStatusAPi = "/status/device/";
+    String url;
+    JSONObject json;
 
     // Ties the device to an address
-    public Device(String address) {
+    public Device(String address) { this.address = address; }
+
+    // Constructs device
+    public Device(String address, String host, String port) {
+
         this.address = address;
+        bridgeURI = "\"http://\"" + deviceStatusAPi +   "\":\" + " + port;
     }
 
     // Get address value
     public String getAddress() {
         return address;
+    }
+
+    public JSONObject getValue() throws JSONException, MalformedURLException, IOException {
+
+        //url = bridgeURI + deviceStatusAPi + "?addr=" + address;
+        url = "http://ip.jsontest.com/";
+        json = new JSONObject(IOUtils.toString(new URL(url), Charset.forName("UTF-8")));
+        return json;
     }
 }
